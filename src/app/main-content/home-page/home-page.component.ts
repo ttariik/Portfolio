34d38a0cda2f93b381a66e1fd735a5c91d4ac7d8
ttariik <tarik.sabanovic2@icloud.com>
2025-02-19 14:42:42 +0100

    fix

--- src/app/main-content/home-page/home-page.component.ts
+++ src/app/main-content/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MarqueeComponent } from '../../shared/marquee/marquee.component';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [MarqueeComponent,
+  imports: [MarqueeComponent,HeaderComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
