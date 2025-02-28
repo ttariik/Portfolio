1e019bf738f80ed11f5932e5844feb29a1edce0a
ttariik <tarik.sabanovic2@icloud.com>
2025-02-28 02:39:44 +0100

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './main-content/home-page/home-page.component';
+import { AboutMeComponent } from './main-content/about-me/about-me.component';
+import { SkillSetComponent } from './main-content/skill-set/skill-set.component';
+import { ProjectsComponent } from './main-content/projects/projects.component';
+import { ContactMeComponent } from './main-content/contact-me/contact-me.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'about', component: AboutMeComponent },
+  { path: 'skills', component: SkillSetComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactMeComponent },
+  { path: 'feedback', component: FeedbackComponent },
+  { path: '**', redirectTo: '' } 
+];
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [SvgIconComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
