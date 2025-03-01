21b975ae900f72787a25548f63768aa23a2555c6
ttariik <tarik.sabanovic2@icloud.com>
2025-03-01 03:25:02 +0100

    implementation of aos library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from './main-content/home-page/home-page.component';
@@ -8,17 +9,39 @@ import { SkillSetComponent } from './main-content/skill-set/skill-set.component'
 import { ProjectsComponent } from './main-content/projects/projects.component';
 import { ContactMeComponent } from './main-content/contact-me/contact-me.component';
 import { FeedbackComponent } from './feedback/feedback.component';
-
-
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomePageComponent, HeaderComponent, AboutMeComponent, SkillSetComponent, ProjectsComponent, ContactMeComponent, FeedbackComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    HomePageComponent, 
+    HeaderComponent, 
+    AboutMeComponent, 
+    SkillSetComponent, 
+    ProjectsComponent, 
+    ContactMeComponent, 
+    FeedbackComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init({
+        duration: 800, 
+        easing: 'ease-in-out', 
+        once: true, 
+        mirror: false 
+      });
+    }
+  }
 }
